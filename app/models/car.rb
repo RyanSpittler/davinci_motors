9ee0ff9a9ed28aa67a7ff4165ff84b70c6c7302f
Ryan Spittler <ryanspittler@gmail.com>
2015-04-01 20:27:07 -0600

    Begin ActiveRecord validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,10 @@
 class Car < ActiveRecord::Base
+  validates :make, :model, :year, :price, presence: true
+  validates :price, numericality: {
+      greater_than: 0.0,
+      less_than: 1_000_000
+    }
+  validates :year, inclusion: 1769..Time.zone.now.year.+(1)
+
+  belongs_to :user
 end
